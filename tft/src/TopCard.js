d5dd9bfcda3117b70f2771b957ddc7cdd66612ca
isabellewilliams628 <72640662+isabellewilliams628@users.noreply.github.com>
2022-07-03 20:47:04 -0500

    Initial files for Home Page
    Home page js and css files creation, and a little bit of formatting and front end designing

---
+++ tft/src/TopCard.js
@@ -0,0 +1,12 @@
+import './TopCard.css';
+
+function TopCard() {
+    return (
+      <div className="TopCard">
+      <div className="TopCardText">TEAMFIGHT TACTICS GUIDE</div>
+      <div className="TopCardImage"></div>
+      </div>
+    );
+  }
+  
+  export default TopCard;
