9afdcbaac8b07711fdcec09fbe7b651c576f87ea
coralrr <104854626+coralrr@users.noreply.github.com>
2022-07-06 11:13:58 -0500

    Create db.js

---
+++ tft/src/db.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "dbuser",
+  password: "ZY3?sLJ7#6Zg#as"
+});
+
+/*con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});*/
