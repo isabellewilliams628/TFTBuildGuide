e5f4060393b9ec85b9baf5a183b5dc8665d408f4
Amina <65184488+aminaopio@users.noreply.github.com>
2022-07-11 09:18:21 -0500

    Add files via upload
    Item page with CSS, to add "<Route path="items" element={<Items />} />" to App.js to view

---
+++ tft/src/Items.js
@@ -0,0 +1,168 @@
+import React from 'react'
+import NavBar from './NavBar.js'
+import './Items.css'
+
+function Items() {
+
+    return(
+        <div className="Items">
+            <NavBar />
+      <table>
+        <tr>
+          <th>Basic Item</th>
+          <th>Tier</th>
+          <th>Item Bonus</th>
+          <th>Combined Items</th>
+        </tr>
+        <tr>
+          <td><img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/bf-sword.png?v=7" alt="B.F. Sword "></img></td>
+          <td><img src="https://cdn.mobalytics.gg/assets/common/icons/hex-tiers/S.svg?2" ></img></td>
+          <td>+10 percent Attack Damage</td>
+          <td><img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/bloodthirster.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/giant-slayer.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/hextech-gunblade.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/infinity-edge.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/spear-of-shojin.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/deathblade.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/edge-of-night.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/shimmerscale-emblem.png?v=7" width="64" height="64"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/zekes-herald.png?v=7"></img>
+        </td>
+        </tr>
+
+        <tr>
+          <td><img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/chain-vest.png?v=7" alt="Chain Vest"></img></td>
+          <td><img src="https://cdn.mobalytics.gg/assets/common/icons/hex-tiers/B.svg?2" ></img></td>
+          <td>+20 Armor</td>
+          <td>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/bramble-vest.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/shroud-of-stillness.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/titans-resolve.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/edge-of-night.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/gargoyle-stoneplate.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/sunfire-cape.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/frozen-heart.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/cavalier-emblem.png?v=7" width="64" height="64"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/locket-of-the-iron-solari.png?v=7" width="64" height="64"></img>
+          </td>
+        </tr>
+
+        <tr>
+          <td><img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/giants-belt.png?v=7" alt="Giants Belt"></img></td>
+          <td><img src="https://cdn.mobalytics.gg/assets/common/icons/hex-tiers/A.svg?2" ></img></td>
+          <td>+150 Health</td>
+          <td>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/morellonomicon.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/redemption.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/banshees-claw.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/sunfire-cape.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/warmogs-armor.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/zzrot-portal.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/zekes-herald.png?v=7"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/guardian-emblem.png?v=7" width="64" height="64"></img>
+          <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/zephyr.png?v=7" width="64" height="64"></img>
+          </td>
+        </tr>
+
+        <tr>
+          <td><img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/needlessly-large-rod.png?v=7" alt="Needlessly Large Rod"></img></td>
+          <td><img src="https://cdn.mobalytics.gg/assets/common/icons/hex-tiers/A.svg?2" ></img></td>
+          <td>+10 Ability Power</td>
+          <td>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/guinsoos-rageblade.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/hextech-gunblade.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/jeweled-gauntlet.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/morellonomicon.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/rabadons-deathcap.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/ionic-spark.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/archangels-staff.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/dragonmancer-emblem.png?v=7" width="64" height="64"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/locket-of-the-iron-solari.png?v=7" width="64" height="64"></img>
+          </td>
+        </tr>
+        <tr>
+          <td><img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/negatron-cloak.png?v=7" alt="Negatron Cloak"></img></td>
+          <td><img src="https://cdn.mobalytics.gg/assets/common/icons/hex-tiers/A.svg?2" ></img></td>
+          <td>+20 Magic Resist</td>
+          <td>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/bloodthirster.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/dragons-claw.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/quicksilver.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/chalice-of-power.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/gargoyle-stoneplate.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/ionic-spark.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/runaans-hurricane.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/mirage-emblem.png?v=7" width="64" height="64"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/zephyr.png?v=7"></img>
+           </td>
+        </tr>
+        <tr>
+          <td><img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/recurve-bow.png?v=7" alt="Recurve Bow"></img></td>
+          <td><img src="https://cdn.mobalytics.gg/assets/common/icons/hex-tiers/S.svg?2" ></img></td>
+          <td>+10% Attack Speed</td>
+          <td>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/giant-slayer.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/guinsoos-rageblade.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/last-whisper.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/statikk-shiv.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/titans-resolve.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/runaans-hurricane.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/rapid-firecannon.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/ragewing-emblem.png?v=7" width="64" height="64"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/zzrot-portal.png?v=7"></img>
+          </td>
+        </tr>
+        <tr>
+          <td><img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/sparring-gloves.png?v=7" alt="Sparring Gloves"></img></td>
+          <td><img src="https://cdn.mobalytics.gg/assets/common/icons/hex-tiers/S.svg?2" ></img></td>
+          <td>+5% Crit Chance, +10% Dodge Chance</td>
+          <td>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/infinity-edge.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/jeweled-gauntlet.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/last-whisper.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/quicksilver.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/shroud-of-stillness.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/banshees-claw.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/hand-of-justice.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/assassin-emblem.png?v=7" width="64" height="64"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/thiefs-gloves.png?v=7"></img>
+          </td>
+        </tr>
+        <tr>
+          <td><img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/spatula.png?v=7" alt="Spatula"></img></td>
+          <td><img src="https://cdn.mobalytics.gg/assets/common/icons/hex-tiers/A.svg?2" ></img></td>
+          <td>It must do something...</td>
+          <td>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/mage-emblem.png?v=7" width="64" height="64"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/tacticians-crown.png?v=7" width="64" height="64"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/assassin-emblem.png?v=7" width="64" height="64"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/cavalier-emblem.png?v=7" width="64" height="64"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/dragonmancer-emblem.png?v=7" width="64" height="64"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/guardian-emblem.png?v=7" width="64" height="64"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/shimmerscale-emblem.png?v=7" width="64" height="64"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/mirage-emblem.png?v=7" width="64" height="64"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/ragewing-emblem.png?v=7" width="64" height="64"></img>
+          </td>
+        </tr>
+        <tr>
+          <td><img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/tear-of-the-goddess.png?v=7" alt="Tear of the Goddess"></img></td>
+          <td><img src="https://cdn.mobalytics.gg/assets/common/icons/hex-tiers/S.svg?2" ></img></td>
+          <td>+15 mana</td>
+          <td>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/blue-buff.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/redemption.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/spear-of-shojin.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/statikk-shiv.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/chalice-of-power.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/archangels-staff.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/hand-of-justice.png?v=7"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/mage-emblem.png?v=7" width="64" height="64"></img>
+           <img src="https://cdn.mobalytics.gg/assets/tft/images/game-items/set7/frozen-heart.png?v=7"></img>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+  
+export default Items
