fba91fbed6080e811c089e0833b9b449ed806b15
coralrr <104854626+coralrr@users.noreply.github.com>
2022-07-18 19:28:53 -0500

    updating registration function

--- tft/src/Registration.js
+++ tft/src/Registration.js
@@ -1,29 +1,56 @@
 import React, {useState} from 'react';
-import './Registration.css'
+import './Registration.css';
+import Axios from 'axios';
 import NavBar from './NavBar';
 
 function Registration(){
-return(
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password,setPassword] = useState('');
 
+    const addUser = () => {
+        Axios.post('http://localhost:3001/register', {
+            email: email,
+            username: username,
+            password: password,
+        }).then(() => {
+            console.log("success");
+        });
+    };
+
+    return(
         <div className="form">
-            <div className="form-body">
-                <div className="email">
-                    <label className="form__label" for="email">E-mail: </label>
-                    <input  type="email" id="email" className="form__input" placeholder="E-mail"/>
-                </div>
-                <div className="username">
-                    <label className="form__label" for="username">Username: </label>
-                    <input className="form__input" type="text" id="username" placeholder="Username"/>
-                </div>
-                <div className="password">
-                    <label className="form__label" for="password">Password: </label>
-                    <input className="form__input" type="password"  id="password" placeholder="Password"/>
-                </div>
+        <div className="form-body">
+            <div className="email">
+                <label className="form__label" for="email">Email: </label>
+                <input  type="email"
+                onChange={(e) => {
+                    setEmail(e.target.value);
+                    }} 
+                    placeholder="Email"/>
             </div>
-            <div class="footer">
-                <button type="submit" class="btn">Sign Up</button>
+            <div className="username">
+                <label className="form__label" for="username">Username: </label>
+                <input className="form__input" type="text"            
+                onChange={(e) => {
+                    setUsername(e.target.value);
+                    }}  
+                    placeholder="Username"/>
             </div>
-        </div>      
-    )       
+            <div className="password">
+                <label className="form__label" for="password">Password: </label>
+                <input className="form__input" type="password"                 
+                onChange={(e) => {
+                    setPassword(e.target.value);
+                    }} 
+                    placeholder="Password"/>
+            </div>
+        </div>
+        <div class="footer">
+            <button onClick={addUser} >Sign Up</button>
+        </div>
+    </div>
+    )
 }
+
 export default Registration;
