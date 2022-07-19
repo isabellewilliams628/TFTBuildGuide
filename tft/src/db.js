bfe383a90e166ed1ef1572fb6b0575ed42741cd2
coralrr <104854626+coralrr@users.noreply.github.com>
2022-07-18 19:28:01 -0500

    updating db connection for registering

--- tft/src/db.js
+++ tft/src/db.js
@@ -1,7 +1,37 @@
-var mysql = require('mysql');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mysql = require('mysql2');
+app.use(express.json());
+app.use(cors());
 
-var con = mysql.createConnection({
+const con = mysql.createConnection({
   host: "localhost",
-  user: "dbuser",
-  password: "ZY3?sLJ7#6Zg#as"
+  user: "root",
+  password: "rdcfgvjhlbni;P01",
+  database: "team_fight_tactics"
 });
+
+
+app.post('/register', (req,res)=> {
+
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    con.query('INSERT INTO users (email, username, password) VALUES (?,?,?)', 
+    [email, username, password],
+    (err,result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send("user added!");
+        }
+    });
+});
+
+app.listen(3001, () => {
+    console.log("server is running!");
+});
+
+module.exports = con;
