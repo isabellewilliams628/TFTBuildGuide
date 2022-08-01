da55a67718def70331a877febf737e3381209b3d
Amina <65184488+aminaopio@users.noreply.github.com>
2022-08-01 16:35:24 -0500

    Update App.js
    Added augment route/gradient bg

--- src/App.js
+++ src/App.js
@@ -5,22 +5,26 @@ import Account from './Account.js'
 import Items from './Items.js'
 import Registration from './Registration.js';
 import Login from './login.js'
+import Augments from './Augments';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
+      <div className="gradient__bg ">
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
       <Route path="items" element={<Items />} />
       <Route path="tierlist" element={<TierList />} />
+      <Route path="Augments" element={<Augments />} />
       <Route path="registration" element={<Registration />} />
       <Route path="login" element={<Login />}/>
       <Route path="account" element={<Account />} />
     </Routes>
     </BrowserRouter>
+    </div>
     </div>
   );
 }
