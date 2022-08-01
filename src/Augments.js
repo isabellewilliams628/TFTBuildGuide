da81cc3ab367a44e97c0da1a29b7016addda8a14
Amina <65184488+aminaopio@users.noreply.github.com>
2022-08-01 16:44:19 -0500

    Add files via upload

---
+++ src/Augments.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import NavBar from './NavBar.js'
+import './Augments.css'
+
+function Augments () {
+
+    return(
+        <div className="Augments">
+            <NavBar />
+    <table>
+        <tr>
+          <th>Augment</th>
+          <th>Tier</th>
+        </tr>
+    </table>
+    </div>
+    );      
+}
+
+    export default Augments;
