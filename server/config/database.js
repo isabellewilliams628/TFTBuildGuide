aa272440b586098c063129b6898d24c37bd1c0f2
coralrr <104854626+coralrr@users.noreply.github.com>
2022-08-07 12:52:00 -0500

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,1 +1,7 @@
+import {Sequelize} from "sequelize";
 
+const db = new Sequelize('team_fight_tactics', 'root', 'rdcfgvjhlbni;P01', {
+    host: "localhost",
+    dialect: "mysql"
+})
+export default db;
