094415918da934dbe97b7c1bc6e91e82ba739dd4
coralrr <104854626+coralrr@users.noreply.github.com>
2022-08-07 13:00:51 -0500

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,1 +1,15 @@
 
+import express from "express";
+import { getUsers, register, LogIn, Logout } from "../controllers/users.js";
+import { verifyToken } from "../controllers/verification.js";
+import { refreshToken } from "../controllers/refresh.js";
+ 
+const router = express.Router();
+ 
+router.get('/users', verifyToken, getUsers);
+router.post('/users', register);
+router.post('/login', LogIn);
+router.get('/token', refreshToken);
+router.delete('/logout', Logout);
+ 
+export default router;
