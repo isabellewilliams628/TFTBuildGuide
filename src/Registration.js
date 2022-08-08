61407c750f71a2b7d374ce94058d209098069a7e
coralrr <104854626+coralrr@users.noreply.github.com>
2022-08-08 09:35:58 -0500

    Update Registration.js

--- src/Registration.js
+++ src/Registration.js
@@ -7,76 +7,79 @@ import Register from './assets/Register.png';
 
 const Registration = () => {
 
-    const [email, setEmail] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [msg, setMsg] = useState('');
+    const initialVals = {
+        email: '',
+        username: '',
+        password: ''
+      };
+
+    const [userInfo, setUserInfo] = useState(initialVals);
     const history = useNavigate();
 
-    const addUser = async (e) => {
-        e.preventDefault();
-        try {
-            await axios.post('https://team-tight-tactics-db.herokuapp.com/users', {
-                username: username,
-                email: email,
-                password: password
-            });
+    const onFormUpdate = (category, value) => {
+        setUserInfo({
+          ...userInfo,
+          [category]: value
+        });
+      };
 
-            history.push("/");
-        } 
-        catch (error) {
-            if (error.response) {
-                setMsg(error.response.data.msg);
-            }
+      const addUser = async (e) => {
+        e.preventDefault();
+
+        let response = await fetch("https://team-tight-tactics-db.herokuapp.com/users", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json;charset=utf-8",
+          },
+          body: JSON.stringify(userInfo),
+        });
+        let result = await response.json();
+        setUserInfo(initialVals);
+        if (result.code == 200) {
+          setStatus({ succes: true, message: 'Message sent successfully' });
+        } else {
+          setStatus({ succes: false, message: 'Something went wrong, please try again later.' });
         }
-    }
+      };
     let navigate = useNavigate(); 
     const redirect = () =>{ 
       let path = `/login`; 
       navigate(path);
     }
 
+
     return(
         <div>
             <NavBar />
         <div className="Images">
             <img src={Register} className="image"></img>
-        <div className="form" >
-            <div className="form-body">
-                <div className="email">
-                    <label className="form__label" for="email">Email: </label>
-                    <input  type="email" value={email}
-                    onChange={(e) => {
-                        setEmail(e.target.value);
-                        }} 
-                        placeholder="Email"/>
+            <div className="form" >
+                <div className='text'>
+                    <label> Sign Up </label>
                 </div>
-                <div className="username">
-                    <label className="form__label" for="username">Username: </label>
-                    <input className="form__input" type="text"   value = {username}         
-                    onChange={(e) => {
-                        setUsername(e.target.value);
-                        }}  
-                        placeholder="Username"/>
+                    <form onSubmit={redirect} className="form-body">
+                        <div className="email">
+                            <label className="form__label" for="email">Email: </label>
+                            <input  type="email" value={userInfo.email} placeholder="Email:" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                        </div>
+                        <div className="username">
+                            <label className="form__label" for="username">Username: </label>
+                            <input className="form__input" type="text"   value={userInfo.username} placeholder="Username:" onChange={(e) => onFormUpdate('username', e.target.value)} />        
+                        </div>
+                        <div className="password">
+                            <label className="form__label" for="password">Password: </label>
+                            <input className="form__input" type="password" vvalue={userInfo.password} placeholder="Password:" onChange={(e) => onFormUpdate('password', e.target.value)} />       
+                        </div>
+                        <div>
+                            <button type="submit" className='button'
+                                onClick={() => {
+                                    addUser();
+                            }}> Sign Up</button>
+                        </div>
+                    </form>
                 </div>
-                <div className="password">
-                    <label className="form__label" for="password">Password: </label>
-                    <input className="form__input" type="password" value={password}         
-                    onChange={(e) => {
-                        setPassword(e.target.value);
-                        }} 
-                        placeholder="*******"/>
-                        <div></div>
-                    <button onClick={(e) => {
-                        addUser(e);
-                        redirect();
-                    }}> Sign Up</button>
-                    
             </div>
-            </div>
-    </div>
-    </div>
-    </div>
+        </div>
     )
 
 };
