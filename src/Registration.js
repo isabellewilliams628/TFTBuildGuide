ea319b8a487faf21d2dc019404f2b4e4a7003aae
coralrr <104854626+coralrr@users.noreply.github.com>
2022-08-09 16:19:59 -0500

    cleaning up form

--- src/Registration.js
+++ src/Registration.js
@@ -23,25 +23,16 @@ const Registration = () => {
         });
       };
 
-      const addUser = async (e) => {
+    const addUser = async (e) => {
         e.preventDefault();
-
-        let response = await fetch("https://team-tight-tactics-db.herokuapp.com/users", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json;charset=utf-8",
-          },
-          body: JSON.stringify(userInfo),
-        });
-        let result = await response.json();
-        setUserInfo(initialVals);
-          history.push("/");
-        if (result.code == 200) {
-          setStatus({ succes: true, message: 'Message sent successfully' });
-        } else {
-          setStatus({ succes: false, message: 'Something went wrong, please try again later.' });
-        }
-      };
+        await axios.post('http://localhost:3000/users', {
+                email: email,
+                username: username,
+                password: password
+            });
+            setUserInfo(initialVals);
+            history.push("/");
+        } 
     let navigate = useNavigate(); 
     const redirect = () =>{ 
       let path = `/login`; 
@@ -58,17 +49,17 @@ const Registration = () => {
                 <div className='text'>
                     <label> Sign Up </label>
                 </div>
-                    <form onSubmit={redirect} className="form-body">
+                    <form onSubmit={addUser} className="form-body">
                         <div className="email">
-                            <label className="form__label" for="email">Email: </label>
+                            <label className="form__label" >Email: </label>
                             <input  type="email" value={userInfo.email} placeholder="Email:" onChange={(e) => onFormUpdate('email', e.target.value)} />
                         </div>
                         <div className="username">
-                            <label className="form__label" for="username">Username: </label>
+                            <label className="form__label" >Username: </label>
                             <input className="form__input" type="text"   value={userInfo.username} placeholder="Username:" onChange={(e) => onFormUpdate('username', e.target.value)} />        
                         </div>
                         <div className="password">
-                            <label className="form__label" for="password">Password: </label>
+                            <label className="form__label" >Password: </label>
                             <input className="form__input" type="password" vvalue={userInfo.password} placeholder="Password:" onChange={(e) => onFormUpdate('password', e.target.value)} />       
                         </div>
                         <div>
