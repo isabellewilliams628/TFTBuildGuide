f573cc162171d7f5d1516f0b4c53631b3af9a83c
coralrr <104854626+coralrr@users.noreply.github.com>
2022-08-09 16:22:18 -0500

    adding username

--- src/Account.js
+++ src/Account.js
@@ -5,29 +5,31 @@ import react, { useEffect, useState } from 'react';
 import profile from './assets/profile.png'
 
 function Account() {
-    return (
-      <div className="Account">
-        <NavBar />
-        <div className="AccoungPage">
-        <div className="TopHalf">
-          <div className="LeftHalf">
-          <div className="ProfilePicture">
-            <img src={profile} className="ProfilePic"></img>
+
+  const userName = sessionStorage.getItem('user');
+   return (
+        <div className="Account">
+          <NavBar />
+          <div className="AccoungPage">
+          <div className="TopHalf">
+            <div className="LeftHalf">
+            <div className="ProfilePicture">
+              <img src={profile} className="ProfilePic"></img>
+            </div>
+            </div>
+            <div className="RightHalf">
+            <div className="Username"> Welcome {userName}!</div>
+            </div>
           </div>
+          <div className="BottomHalf">
+            <div className="FavBuildsBar">
+              <div className="FavBuildsBarText">FAVORITE BUILDS: </div>
+            </div>
+            <div className="FavBuilds"></div>
           </div>
-          <div className="RightHalf">
-          <div className="Username">Welcome!</div>
           </div>
         </div>
-        <div className="BottomHalf">
-          <div className="FavBuildsBar">
-            <div className="FavBuildsBarText">FAVORITE BUILDS: </div>
-          </div>
-          <div className="FavBuilds"></div>
-        </div>
-        </div>
-      </div>
-    );
+      )
   }
   
   export default Account;
