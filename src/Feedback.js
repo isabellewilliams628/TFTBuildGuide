81e72efa96a0f364a2a0afe4970221a07529ccce
coralrr <104854626+coralrr@users.noreply.github.com>
2022-08-10 11:16:43 -0500

    Update Feedback.js

--- src/Feedback.js
+++ src/Feedback.js
@@ -4,15 +4,11 @@ import contactImg from "./assets/feedback.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 import NavBar from './NavBar';
-import './Feedback.css'
-import axios from 'axios';
+import './Feedback.css';
 
 function Feedback() {
   const formInitialDetails = {
-    firstName: '',
-    lastName: '',
     email: '',
-    phone: '',
     message: ''
   };
   const [formDetails, setFormDetails] = useState(formInitialDetails);
@@ -26,10 +22,11 @@ function Feedback() {
     });
   };
 
+ 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch('https://team-tight-tactics-db.herokuapp.com/feedback', {
+    let response = await fetch("https://team-tight-tactics-db.herokuapp.com/api/feedback", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
@@ -64,16 +61,7 @@ function Feedback() {
                 <form onSubmit={handleSubmit}>
                   <Row>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
                       <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
                     </Col>
                     <Col size={12} className="px-1">
                       <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
